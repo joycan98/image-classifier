7f94d83487de3c9e12ecf86bf10db17ec01799c7
joycan98 <73891921+joycan98@users.noreply.github.com>
2021-04-04 13:02:38 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let img;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('/image-classifier/images/bird.png');
+  img = loadImage('uploaded_image');
 }
 
 function setup() {
